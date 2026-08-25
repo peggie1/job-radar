ec1b84edd3d5715109963b4205e4f7f5d4566990
peggie1 <751772465@qq.com>
2026-08-25 21:24:04 +0800

    Add sw.js

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE='job-radar-v1';
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','index.html','manifest.webmanifest']))));
+self.addEventListener('fetch',e=>{if(new URL(e.request.url).pathname.endsWith('jobs.json'))return;e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))})
+

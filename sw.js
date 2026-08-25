b426a367bc9f8c54cc0d7cebf09a52812a5e7ba7
peggie1 <751772465@qq.com>
2026-08-25 22:21:39 +0800

    Refresh app cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='job-radar-v3';
+const CACHE='job-radar-v4';
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','index.html','manifest.webmanifest']))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener('fetch',e=>{if(new URL(e.request.url).pathname.endsWith('jobs.json'))return;e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))})

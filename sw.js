e3ff291740808cf789a09afd8e235f53920ff65a
peggie1 <751772465@qq.com>
2026-08-26 16:57:19 +0800

    Polish job radar app experience

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='job-radar-v4';
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','index.html','manifest.webmanifest']))));
+const CACHE='job-radar-v5';
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','index.html','manifest.webmanifest','icon.svg'])).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener('fetch',e=>{if(new URL(e.request.url).pathname.endsWith('jobs.json'))return;e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))})
+self.addEventListener('fetch',e=>{if(new URL(e.request.url).pathname.endsWith('jobs.json')){e.respondWith(fetch(e.request,{cache:'no-store'}));return}e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))})
 

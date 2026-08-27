96e5a327de25754707028b01855ade87e379acf7
peggie1 <751772465@qq.com>
2026-08-27 10:53:55 +0800

    Refresh app cache for salary filter

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='job-radar-v5';
+const CACHE='job-radar-v6';
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','index.html','manifest.webmanifest','icon.svg'])).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{if(new URL(e.request.url).pathname.endsWith('jobs.json')){e.respondWith(fetch(e.request,{cache:'no-store'}));return}e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))})
 
